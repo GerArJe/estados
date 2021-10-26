2213559dd18e4e5b57d8c23784c4a843956d6728
German Arevalo J <germanarevalojerez@gmail.com>
2021-10-26 15:17:44 -0500

    provider listo

--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -1,20 +1,38 @@
+import 'package:estados/models/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:estados/services/usuario_service.dart';
 
 class Pagina2Page extends StatelessWidget {
   const Pagina2Page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final usuarioService = Provider.of<UsuarioService>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pagina 2'),
+        title: usuarioService.existeUsuario
+            ? Text('Nombre: ${usuarioService.usuario.nombre}')
+            : const Text('Pagina 2'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             MaterialButton(
-              onPressed: () {},
+              onPressed: () {
+                final newUser = Usuario(
+                  nombre: 'Fernando Herrera',
+                  edad: 35,
+                  /* profesiones: [
+                    'Fullstack developer',
+                    'video jugador experto',
+                  ], */
+                );
+                usuarioService.usuario = newUser;
+              },
               child: const Text(
                 'Establecer Usuario',
                 style: TextStyle(
@@ -24,7 +42,9 @@ class Pagina2Page extends StatelessWidget {
               color: Colors.blue,
             ),
             MaterialButton(
-              onPressed: () {},
+              onPressed: () {
+                usuarioService.cambiarEdad(45);
+              },
               child: const Text(
                 'Cambiar edad',
                 style: TextStyle(
@@ -34,7 +54,7 @@ class Pagina2Page extends StatelessWidget {
               color: Colors.blue,
             ),
             MaterialButton(
-              onPressed: () {},
+              onPressed: () => usuarioService.agregarProfesion(),
               child: const Text(
                 'Añadir profesion',
                 style: TextStyle(
